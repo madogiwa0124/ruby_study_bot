0caff7b9da1b81dc7df227d0c9ae4a64f8fceddb
Madogiwa <17684091+Madogiwa0124@users.noreply.github.com>
2018-09-15 16:10:26 +0900

    RubyMagazineクラスをリファクタリング

--- ruby_magazine.rb
+++ ruby_magazine.rb
@@ -1,37 +1,37 @@
-require 'open-uri'
-require 'nokogiri'
-
 class RubyMagazine
+  require 'open-uri'
+  require 'nokogiri'
 
   URL = 'http://magazine.rubyist.net/'
 
-  def initialize
-  end
+  class << self
 
-  def get_magazine_page
-    top_page = get_page_parsed_object(URL)
-    get_magazine_url_list = get_magazine_url_list(top_page)
-    get_magazine_url_list.sample
-  end
-
-  def get_magazine_url_list(top_page)
-    top_page_links = top_page.css('div.body').css('div.section').css('ul li a')
-    top_page_links = top_page_links.map do |link|
-      { 
-        url:   URL + link.attribute('href').value.sub('./',''),
-        title: link.text
-      }
+    def get_magazine_page
+      top_page = get_page_parsed_object(URL)
+      get_magazine_url_list = get_magazine_url_list(top_page)
+      get_magazine_url_list.sample
     end
-    top_page_links.select{ |link| link[:url].match(/#{URL}\?\d.*/) }
-  end
 
-  def get_page_parsed_object(url)
-    charset = nil    
-    html = open(url) do |f|
-      charset = f.charset
-      f.read
+    private
+
+    def get_magazine_url_list(top_page)
+      top_page_links = top_page.css('div.main').css('ul li a')
+      top_page_links = top_page_links.map do |link|
+        {
+          url:   URL + link.attribute('href').value.sub('./',''),
+          title: link.text
+        }
+      end
+      top_page_links.select{ |link| link[:url].match?(/articles/) }
     end
-    doc = Nokogiri::HTML.parse(html, nil, charset)
-    doc
+
+    def get_page_parsed_object(url)
+      charset = nil
+      html = open(url) do |f|
+        charset = f.charset
+        f.read
+      end
+      Nokogiri::HTML.parse(html, nil, charset)
+    end
   end
 end
