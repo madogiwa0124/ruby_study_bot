facd4a3ea17131699b573d8efda148045bc6bb7f
Madogiwa <17684091+Madogiwa0124@users.noreply.github.com>
2018-09-15 15:55:00 +0900

    RubyReferenceManualクラスをリファクタリング

--- ruby_manual.rb
+++ ruby_manual.rb
@@ -1,50 +1,72 @@
-require 'open-uri'
-require 'nokogiri'
-
 class RubyReferenceManual
-
-  attr_reader :class_list
+  require 'open-uri'
+  require 'nokogiri'
 
   RUBY_VERSION = "2.5.0"
   MANUAL_URL = "https://docs.ruby-lang.org/ja/#{RUBY_VERSION}/class/"
   CLASS_LIST = [String, Numeric, Array, Hash, IO]
 
-  def initialize
-    @class_list = CLASS_LIST
-  end
-
-  def get_class_page(target_class)
-    unless @class_list.include?(target_class)
-      raise "クラスは#{@class_list.join(',')}のいずれかを指定してください。"
+  class << self
+    def sample_target
+      target_class = CLASS_LIST.sample
+      method = method_list(target_class).sample
+      id = "#I_#{url_encode_text(method.to_s.upcase)}"
+      {
+        class:  target_class,
+        method: method,
+        manual: "#{MANUAL_URL}#{target_class}.html#{id}"
+      }
     end
-    url = "#{MANUAL_URL}/#{target_class}.html"
-    class_page = get_page_parsed_object(url)
-    class_page
-  end
 
-  def method_list(target_class)
-    class_page = get_class_page(target_class)
-    methods = class_page.css('dl').css('dd').css('a').map{ |m| m.text }
-    # 開始文字が不正なものは除外
-    methods = methods.grep(/^([a-z]|%|\*|\+|<|=|\+|\[).*/)
-    # [数字を含む文字列]、(数字を含む文字列)が含まれているものは除外
-    methods -= methods.grep(/(\(|\[).*\d(\)|\])/)
-    # http始まりのものは削除
-    methods -= methods.grep(/http.*/)
-    # 日本語が含まれるものは削除
-    methods -= methods.grep(/[^\x01-\x7E]/)
-    # 正規表現で判定出来ないため苦肉の策でbase64は直接削除
-    methods.delete('base64')
-    methods
-  end
+    private
 
-  def get_page_parsed_object(url)
-    charset = nil    
-    html = open(url) do |f|
-      charset = f.charset
-      f.read
+    def method_list(target_class)
+      class_page = class_page(target_class)
+      methods = class_page.css('dl').css('dd').css('a').map{ |m| m.text }
+      # 開始文字が不正なものは除外
+      methods = methods.grep(/^([a-z]|%|\*|\+|<|=|\+|\[).*/)
+      # [数字を含む文字列]、(数字を含む文字列)が含まれているものは除外
+      methods -= methods.grep(/(\(|\[).*\d(\)|\])/)
+      # http始まりのものは削除
+      methods -= methods.grep(/http.*/)
+      # 日本語が含まれるものは削除
+      methods -= methods.grep(/[^\x01-\x7E]/)
+      # 正規表現で判定出来ないため苦肉の策でbase64は直接削除
+      methods.delete('base64')
+      methods
     end
-    doc = Nokogiri::HTML.parse(html, nil, charset)
-    doc
+
+    def class_page(target_class)
+      unless CLASS_LIST.include?(target_class)
+        raise "クラスは#{CLASS_LIST.join(',')}のいずれかを指定してください。"
+      end
+      url = "#{MANUAL_URL}/#{target_class}.html"
+      page_parsed_object(url)
+    end
+
+    def page_parsed_object(url)
+      charset = nil
+      html = open(url) do |f|
+        charset = f.charset
+        f.read
+      end
+      Nokogiri::HTML.parse(html, nil, charset)
+    end
+
+    def url_encode_text(text)
+      text
+      .gsub('?','--3F')
+      .gsub('=','--3D')
+      .gsub('<','--3C')
+      .gsub('>','--3E')
+      .gsub('!','--21')
+      .gsub('%','--25')
+      .gsub('@','--40')
+      .gsub('[','--5b')
+      .gsub(']','--5d')
+      .gsub('~','--7e')
+      .gsub('*','--2a')
+      .gsub('+','--2b')
+    end
   end
 end
